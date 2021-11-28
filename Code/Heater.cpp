05d790538650d06de98a6101aeda161236b206f7
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:33:22 +0200

    Code Version for Arduino

--- Code/Heater.cpp
+++ Code/Heater.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include "Heater.h"
 
+//Initializng heater pins as output
 void Heater_Init(void)
 {
   pinMode(HEATER1,OUTPUT);
   pinMode(HEATER2,OUTPUT);
   pinMode(HEATER3,OUTPUT);
 }
+
+//Output volatge according to input temperature
 void Heater(int temp)
 {
   analogWrite(HEATER1,((temp/MAX_TEMP)*255.0));
