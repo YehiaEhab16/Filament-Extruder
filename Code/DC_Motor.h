3e55f82d10b95bb33b037f2289ce0d66de78a5c7
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:30:16 +0200

    Code Version for Arduino

--- Code/DC_Motor.h
+++ Code/DC_Motor.h
@@ -1,9 +1,9 @@
 #ifndef DC_MOTOR_H_
 #define DC_MOTOR_H_
 
-#define DCM         6
+#define DCM         6       //PWM pin for motor
 
-#define MAX_RPM     200.0
+#define MAX_RPM     200.0   //Max motor RPM
 
 void Motor_Init(void);
 void Motor_Rotate(int rpm);
