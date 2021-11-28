f48ce9cda418535d0ccee27645b042285e285894
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:34:50 +0200

    Code Version for Arduino

--- Code/LCD.h
+++ Code/LCD.h
@@ -1,9 +1,9 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-#define WELCOME_DELAY     3000
+#define WELCOME_DELAY     3000    //Delay for welcome string at the startup
 
-void LCD_Init(void);
+void LCD_Init(void); 
 void LCD_Display(const String string);
 void LCD_DisplayExactPosition(const String string,int x,int y);
 
