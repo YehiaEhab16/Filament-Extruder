9417189f2840483aa4f3c74cd34ffb0a8db07215
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:31:29 +0200

    Code Version for Arduino

--- Code/DC_Motor.cpp
+++ Code/DC_Motor.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include "DC_Motor.h"
 
+//Initialize motor pin as output
 void Motor_Init(void)
 {
   pinMode(DCM,OUTPUT);
 }
+
+//Output volatge according to input RPM
 void Motor_Rotate(int rpm)
 {
   analogWrite(DCM,((rpm/MAX_RPM)*255.0));
