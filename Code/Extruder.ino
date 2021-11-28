8b2f12641f8eac4d1fda67be43bddc75cc32fe99
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:50:53 +0200

    Code Version for Arduino

--- Code/Extruder.ino
+++ Code/Extruder.ino
@@ -1,18 +1,23 @@
+//Including all defined libraries
 #include "DC_Motor.h"
 #include "LCD.h"
 #include "RotaryEncoder.h"
 #include "Heater.h"
 #include "Thermocouple.h"
 
+//Rotary encoder ISRs
 void SW_ISR(void);
 void CLK_ISR(void);
 
+//Volatile variables to be modified inside ISRs
 volatile bool op_flag = OPERATION;
 volatile int st_flag = 0;
-int act_temp = 0;
-volatile int temp = 101, rpm = 150;
+
+int act_temp = 0;                     //actual tempertaure to be read by thermocouple
+volatile int temp = 101, rpm = 150;   //Output temperatue and RPM
 
 void setup() {
+  //Initializing all components
   LCD_Init();
   Encoder_Init(&SW_ISR,&CLK_ISR);
   Heater_Init();
@@ -20,18 +25,22 @@ void setup() {
 }
 
 void loop() {
+  //Displaying variables on LCD
   LCD_DisplayExactPosition(String(act_temp), 5, 0);
   LCD_Display("/");
   LCD_Display(String(temp));
   LCD_Display(" ");
   LCD_DisplayExactPosition(String(rpm), 4, 1);
   LCD_Display(" ");
+  //Output only in operation mode
   if(op_flag==OPERATION)
   {
     Motor_Rotate(rpm);
     Heater(temp);
   }
+  //Reading Temperature from thermocouple
   act_temp = ThermpocoupleRead();
+  //Specifying place where user can edit temp and rpm
   if(op_flag==MODIFY)
   {
     if(st_flag==0)
@@ -48,17 +57,18 @@ void loop() {
     LCD_DisplayExactPosition("   ",12,1);
 }
 
+//ISR for SW pin (determine operation or modify mode)
 void SW_ISR(void)
 {
   Serial.println("Interrupt");
   if (op_flag == OPERATION)
   {
+    //Disabling heaters and motor while modification
     Heater(0);
     Motor_Rotate(0);
     Serial.println("Switched to modify mode");
     op_flag = MODIFY;
   }
-
   else
   {
     if (st_flag == 1)
@@ -71,14 +81,15 @@ void SW_ISR(void)
       st_flag += 1;
   }
 }
+//ISR for CLK pin (determine which direction the encoder is rotating)
 void CLK_ISR(void)
 {
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
 
-  // If interrupts come faster than 5ms, assume it's a bounce and ignore
+  // If interrupts come faster than 20ms, assume it's a bounce and ignore
   if (interruptTime - lastInterruptTime > 20) {
-    if (digitalRead(DT) == LOW)
+    if (Encoder_DT_value() == LOW)
     {
       Serial.println("Right Turn");
       if(st_flag==0 && op_flag==MODIFY)
@@ -93,7 +104,8 @@ void CLK_ISR(void)
       else if(st_flag==1)
         rpm--;
     }
-  lastInterruptTime = interruptTime;
-  }
+    //Updating lastinterrupt value
+    lastInterruptTime = interruptTime;
+    }
 }
 
