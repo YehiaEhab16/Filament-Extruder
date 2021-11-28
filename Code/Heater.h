43230cf20c151838e2202fcf073d5887db338802
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-11-28 02:32:19 +0200

    Code Version for Arduino

--- Code/Heater.h
+++ Code/Heater.h
@@ -1,11 +1,12 @@
 #ifndef HEATER_H_
 #define HEATER_H_
 
+//Heaters pins
 #define HEATER1     9
 #define HEATER2     10
 #define HEATER3     11
 
-#define MAX_TEMP    400.0
+#define MAX_TEMP    400.0   //Max heater temperature
 
 void Heater_Init(void);
 void Heater(int temp);
